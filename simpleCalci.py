82d89df4c63eaa714c88f396b96db8920b4dcb84
Shabeer <shabeer.one@gmail.com>
2024-01-08 14:22:48 +0530

    simpleCalci.py file have class of add and sub

---
+++ simpleCalci.py
@@ -0,0 +1,13 @@
+class calci:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calsum(self):
+        c = self.a+self.b
+        return c
+    
+    def calsub(self):
+        c = self.a-self.b
+        return c
